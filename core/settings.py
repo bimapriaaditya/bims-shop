f92cc4ac69e1131618c03ce8282a0ddd0efdfbba
bimapriaadtiya <bpabima@gmail.com>
2023-06-16 11:23:06 +0700

    add new app

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    'account',
+    'chat',
+    'discount',
+    'forum',
+    'order',
+    'product'
 ]
 
 MIDDLEWARE = [
@@ -108,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'id-ID'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
@@ -121,10 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = config.STATIC_URL
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = config.MEDIA_URL
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
